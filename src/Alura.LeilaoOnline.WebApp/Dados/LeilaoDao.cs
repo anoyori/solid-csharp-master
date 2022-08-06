c493a98459ec68783113310d5c8d8cf512fc5c59
anoyori <anoyori@gmail.com>
2022-08-06 18:52:19 -0300

    refac: alterado método privado para publico

--- src/Alura.LeilaoOnline.WebApp/Dados/LeilaoDao.cs
+++ src/Alura.LeilaoOnline.WebApp/Dados/LeilaoDao.cs
@@ -1,43 +1,47 @@
 ﻿using Alura.LeilaoOnline.WebApp.Models;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Alura.LeilaoOnline.WebApp.Dados
 {
     public class LeilaoDao
     {
-        public Leilao(AppDbContext context)
+        AppDbContext _context;
+
+        public LeilaoDao(AppDbContext context)
         {
-            AppDbContext _context
+            _context = context;
         }
 
-        private IEnumerable<Categoria> BuscarCategorias()
+        public IEnumerable<Categoria> BuscarCategorias()
         {
             return _context.Categorias.ToList();
         }
 
-        private Leilao BuscarPorId(int id)
+        public Leilao BuscarPorId(int id)
         {
             return _context.Leiloes.First(x => x.Id == id);
         }
 
-        private IEnumerable<Leilao> BuscarLeiloes()
+        public IEnumerable<Leilao> BuscarLeiloes()
         {
             return _context.Leiloes.Include(l => l.Categoria).ToList();
         }
 
-        private void Incluir(Leilao leilao)
+        public void Incluir(Leilao leilao)
         {
             _context.Leiloes.Add(leilao);
             _context.SaveChanges();
         }
 
-        private void Excluir(Leilao leilao)
+        public void Excluir(Leilao leilao)
         {
             _context.Leiloes.Remove(leilao);
             _context.SaveChanges();
         }
 
-        private void Alterar(Leilao leilao)
+        public void Alterar(Leilao leilao)
         {
             _context.Leiloes.Update(leilao);
             _context.SaveChanges();
